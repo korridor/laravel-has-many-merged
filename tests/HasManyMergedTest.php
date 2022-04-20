f14bf8cc2310103eb260164f476ed91e557aba37
korridor <26689068+korridor@users.noreply.github.com>
2022-04-20 13:36:51 +0200

    Fixed aggregates; Added tests for withCount, withSum and whereHas

--- tests/HasManyMergedTest.php
+++ tests/HasManyMergedTest.php
@@ -2,13 +2,14 @@
 
 namespace Korridor\LaravelHasManyMerged\Tests;
 
+use Illuminate\Database\Eloquent\Builder;
 use Korridor\LaravelHasManyMerged\HasManyMerged;
 use Korridor\LaravelHasManyMerged\Tests\Models\Message;
 use Korridor\LaravelHasManyMerged\Tests\Models\User;
 
 class HasManyMergedTest extends TestCase
 {
-    private function createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages()
+    private function createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages(): void
     {
         User::create([
             'id' => 11,
@@ -26,28 +27,32 @@ class HasManyMergedTest extends TestCase
             'content' => 'A - This is a message!',
             'sender_user_id' => 1,
             'receiver_user_id' => 1,
+            'content_integer' => 1,
         ]);
         Message::create([
             'id' => 2,
             'content' => 'B - This is a message!',
             'sender_user_id' => 1,
             'receiver_user_id' => 2,
+            'content_integer' => 1,
         ]);
         Message::create([
             'id' => 3,
             'content' => 'C - This is a message!',
             'sender_user_id' => 2,
             'receiver_user_id' => 1,
+            'content_integer' => 1,
         ]);
         Message::create([
             'id' => 4,
             'content' => 'D - This is a message!',
             'sender_user_id' => 2,
             'receiver_user_id' => 2,
+            'content_integer' => 2,
         ]);
     }
 
-    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithLazyLoading()
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithLazyLoading(): void
     {
         // Arrange
         $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
@@ -71,7 +76,7 @@ class HasManyMergedTest extends TestCase
         $this->assertEquals(2, $user2->sentMessages()->count());
     }
 
-    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoading()
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoading(): void
     {
         // Arrange
         $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
@@ -96,7 +101,83 @@ class HasManyMergedTest extends TestCase
         $this->assertEquals(2, $user2->sentMessages()->count());
     }
 
-    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithLazyEagerLoading()
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoadingWithCount(): void
+    {
+        // Arrange
+        $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
+
+        // Act
+        $this->db::connection()->enableQueryLog();
+        $users = User::with(['messages'])->withCount(['messages', 'sentMessages'])->get();
+        $user1 = $users->firstWhere('id', 11);
+        $user2 = $users->firstWhere('id', 12);
+        $messagesOfUser1 = $user1->messages;
+        $messagesOfUser2 = $user2->messages;
+        $queries = $this->db::getQueryLog();
+        $this->db::connection()->disableQueryLog();
+
+        // Assert
+        $this->assertEquals(2, count($queries));
+        $this->assertEquals(3, $messagesOfUser1->count());
+        $this->assertEquals(3, $messagesOfUser2->count());
+        $this->assertEquals(3, $user1->messages_count);
+        $this->assertEquals(3, $user2->messages_count);
+        $this->assertEquals(2, $user1->receivedMessages()->count());
+        $this->assertEquals(2, $user1->sentMessages()->count());
+        $this->assertEquals(2, $user2->receivedMessages()->count());
+        $this->assertEquals(2, $user2->sentMessages()->count());
+    }
+
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoadingWithSum(): void
+    {
+        if (! method_exists(Builder::class, 'withSum')) {
+            $this->markTestSkipped('withSum is not supported');
+        }
+
+        // Arrange
+        $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
+
+        // Act
+        $this->db::connection()->enableQueryLog();
+        $users = User::with(['messages'])->withSum('messages', 'content_integer')->get();
+        $user1 = $users->firstWhere('id', 11);
+        $user2 = $users->firstWhere('id', 12);
+        $messagesOfUser1 = $user1->messages;
+        $messagesOfUser2 = $user2->messages;
+        $queries = $this->db::getQueryLog();
+        $this->db::connection()->disableQueryLog();
+
+        // Assert
+        $this->assertEquals(2, count($queries));
+        $this->assertEquals(3, $messagesOfUser1->count());
+        $this->assertEquals(3, $messagesOfUser2->count());
+        $this->assertEquals(3, $user1->messages_sum_content_integer);
+        $this->assertEquals(4, $user2->messages_sum_content_integer);
+        $this->assertEquals(2, $user1->receivedMessages()->count());
+        $this->assertEquals(2, $user1->sentMessages()->count());
+        $this->assertEquals(2, $user2->receivedMessages()->count());
+        $this->assertEquals(2, $user2->sentMessages()->count());
+    }
+
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoadingWhereHas(): void
+    {
+        // Arrange
+        $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
+
+        // Act
+        $this->db::connection()->enableQueryLog();
+        $users = User::with(['messages'])->whereHas('messages', function (Builder $builder) {
+            $builder->where('content_integer', '=', 2);
+        })->get();
+        $queries = $this->db::getQueryLog();
+        $this->db::connection()->disableQueryLog();
+
+        // Assert
+        $this->assertEquals(1, count($users));
+        $this->assertEquals(2, $users->first()->other_unique_id);
+    }
+
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithLazyEagerLoading(): void
     {
         // Arrange
         $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
@@ -122,7 +203,7 @@ class HasManyMergedTest extends TestCase
         $this->assertEquals(2, $user2->sentMessages()->count());
     }
 
-    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithConstrainedEagerLoading()
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithConstrainedEagerLoading(): void
     {
         // Arrange
         $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
@@ -151,7 +232,7 @@ class HasManyMergedTest extends TestCase
         $this->assertEquals(2, $user2->sentMessages()->count());
     }
 
-    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoadingAndOrderCheck()
+    public function testHasManyMergedWithTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessagesWithEagerLoadingAndOrderCheck(): void
     {
         // Arrange
         $this->createTwoUsersWereBothAreSenderOrReceiverOfTheSameFourMessages();
